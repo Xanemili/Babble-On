2893a81e909ab0ade9d9b731186c991ac0b3a288
Xander <emili.xander@gmail.com>
2020-10-05 13:02:34 -0800

    setting up the user routes

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/log-in', (req,res) => {
+router.get('/log-in', (req, res) => {
   res.render('log-in')
 })
 
+router.get('/sign-up', (req, res) => {
+  res.render('sign-up')
+})
 
 module.exports = router;
