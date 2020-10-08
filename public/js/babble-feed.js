eabdf6138dc2056585799bc343aa6170df77baa0
Christian <lovernchristian@gmail.com>
2020-10-08 17:09:35 -0400

    Search functionality complete

--- public/js/babble-feed.js
+++ public/js/babble-feed.js
@@ -1,3 +1,46 @@
+const searchVal = document.querySelector('.search__bar')
+const searchbtn = document.querySelector('.nav__search');
+searchbtn.addEventListener('click', async() => {
+  try{
+  const searchObj = searchVal.value;
+
+  const res = await fetch(`/api/babbles/search/${searchObj}`);
+  const search = await res.json();
+  } catch (err) {
+    if (err.status >= 400 && err.status < 600) {
+      const errorJSON = await err.json();
+      const errorsContainer = document.querySelector('.errors-container');
+      logo.setAttribute("class", "logo-left")
+      instructions.innerHTML = 'There seems to be some issues, please refer to the instructions above'
+      let errorsHtml = [
+        `
+                <div class="error-alert">
+                    Something went wrong. Please try again.
+                </div>
+              `,
+      ];
+
+      const {
+        errors
+      } = errorJSON;
+      if (errors && Array.isArray(errors)) {
+        errorsHtml = errors.map(
+          (message) => `
+                    <div class "error-alert">
+                        ${message}
+                    </div> `
+        );
+      }
+      errorsContainer.innerHTML = errorsHtml.join("");
+    } else {
+      alert("Something went wrong. Please check your internet connection and try again!")
+    }
+  }
+})
+
+
+
+
 window.addEventListener('DOMContentLoaded', async (e) => {
     try {
         const res1 = await fetch('/api/babbles');
